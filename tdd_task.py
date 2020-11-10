2e52cdb5cc289f580f27cd2adc56d9bc042cb569
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-10 12:28:15 +0000

    Improved pseudocoding

--- tdd_task.py
+++ tdd_task.py
@@ -3,14 +3,14 @@
 # Creating the code class for the modulus/non_zero task
 class Modulus_Calc:
 
-    # Function that output True if value1 / value 2 has NO remainder
+    # Function that outputs True if value1 / value 2 has NO remainder i.e. == 0
     def modulus(self, value1, value2):
         if value1 % value2 == 0:
             return True
         else:
             return False
 
-    # Function that outputs False if any value is < 0, True if it is above 0
+    # Function that outputs True if both values are > 0, False if < 0
     def not_zero(self, value1, value2):
         if value1 and value2 > 0:
             return True
