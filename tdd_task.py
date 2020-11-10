2df7cd28d4f2f5b4db7b0f1b49660cc7071e4149
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-10 13:13:05 +0000

    Changed code in task file

--- tdd_task.py
+++ tdd_task.py
@@ -3,12 +3,9 @@
 # Creating the code class for the modulus/non_zero task
 class Modulus_Calc:
 
-    # Function that outputs True if value1 / value 2 has NO remainder i.e. == 0
+    # Function that outputs the modulus of value1 % value2
     def modulus(self, value1, value2):
-        if value1 % value2 == 0:
-            return True
-        else:
-            return False
+        return value1 % value2
 
     # Function that outputs True if both values are > 0, False if < 0
     def not_zero(self, value1, value2):
